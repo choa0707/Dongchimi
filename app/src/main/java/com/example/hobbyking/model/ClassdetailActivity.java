2698c4e7d73ccaf7a70c3193e76fcccaecce9810
choa0707 <choa0707@naver.com>
2019-05-11 20:25:28 +0900

    Class detail 프레그먼트 추가

--- app/src/main/java/com/example/hobbyking/model/ClassdetailActivity.java
+++ app/src/main/java/com/example/hobbyking/model/ClassdetailActivity.java
@@ -1,15 +1,64 @@
 package com.example.hobbyking.model;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.hobbyking.R;
+import com.example.hobbyking.fragment.ClassinfoFragment;
+import com.example.hobbyking.fragment.ClassreviewFragment;
+import com.example.hobbyking.fragment.ClasstimeFragment;
+import com.example.hobbyking.fragment.TutorinfoFragment;
 
 public class ClassdetailActivity extends AppCompatActivity {
+
+    CustomSwipeAdapter adapter;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_classdetail);
+        viewPager = (ViewPager)findViewById(R.id.view_pager);
+        adapter = new CustomSwipeAdapter(this);
+        viewPager.setAdapter(adapter);
+
+        Button button_classinfo = (Button)findViewById(R.id.classInfo);
+        Button button_tutorinfo = (Button)findViewById(R.id.tutorInfo);
+        Button button_classtime = (Button)findViewById(R.id.classTime);
+        Button button_classreview = (Button)findViewById(R.id.classReview);
+
+        button_classinfo.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.classFrame, new ClassinfoFragment()).commit();
+            }
+        });
+
+        button_classreview.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.classFrame, new ClassreviewFragment()).commit();
+            }
+        });
+
+        button_classtime.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.classFrame, new ClasstimeFragment()).commit();
+            }
+        });
+
+        button_tutorinfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.classFrame, new TutorinfoFragment()).commit();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/hobbyking/model/CustomSwipeAdapter.java
@@ -0,0 +1,50 @@
+package com.example.hobbyking.model;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.hobbyking.R;
+
+public class CustomSwipeAdapter extends PagerAdapter {
+
+    private int[] images = {R.drawable.one, R.drawable.two, R.drawable.three};
+    private LayoutInflater inflater;
+    private Context context;
+
+    public CustomSwipeAdapter(Context context)
+    {
+        this.context = context;
+    }
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+        return view == ((LinearLayout)o);
+    }
+
+    public Object instantiateItem(ViewGroup container, int position)
+    {
+        inflater  = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflater.inflate(R.layout.swipe_layout, container, false);
+        ImageView imageView = (ImageView)v.findViewById(R.id.imageView);
+        imageView.setImageResource(images[position]);
+        container.addView(v);
+        return v;
+    }
+
+    public void destroyItem(ViewGroup container, int position, Object object)
+    {
+        container.invalidate();
+    }
+}
