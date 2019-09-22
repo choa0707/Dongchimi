0270613f574b2226aeb27c096b5805b267646138
choa0707 <choa0707@naver.com>
2019-09-22 15:44:20 +0900

    9/21 회원가입시 카테고리 추가

--- app/src/main/java/com/example/hobbyking/model/SignupActivity2.java
+++ app/src/main/java/com/example/hobbyking/model/SignupActivity2.java
@@ -22,15 +22,18 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class SignupActivity2 extends AppCompatActivity {
 
-    ToggleButton button1,button2,button3,button4,button5,button6,button7,button8,button9,button10,button11;
+    ToggleButton classToggleButton1, classToggleButton2,classToggleButton3,classToggleButton4,classToggleButton5,classToggleButton6,classToggleButton7,classToggleButton8,classToggleButton9,classToggleButton10,classToggleButton11;
+    String classCategoryString="";
     ImageButton signupback;
     Button signup_finish;
     long mNow;
     Date mDate;
+    ArrayList<Integer> classCategory = new ArrayList<>();
     //SimpleDateFormat mFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 
     class CustomTask extends AsyncTask<String, Void, String> {
@@ -45,7 +48,7 @@ public class SignupActivity2 extends AppCompatActivity {
                 conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
                 conn.setRequestMethod("POST");//데이터를 POST 방식으로 전송합니다.
                 OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream());
-                sendMsg = "id="+strings[0]+"&pwd="+strings[1]+"&name="+strings[2]+"&gender="+strings[3]+"&type="+strings[4];//보낼 정보 id=rain483&pwd=1234";
+                sendMsg = "id="+strings[0]+"&pwd="+strings[1]+"&name="+strings[2]+"&gender="+strings[3]+"&classCategory="+strings[4]+"&type="+strings[5];//보낼 정보 id=rain483&pwd=1234";
 
                 osw.write(sendMsg);//OutputStreamWriter에 담아 전송
                 osw.flush();
@@ -97,7 +100,20 @@ public class SignupActivity2 extends AppCompatActivity {
         String gender = intent.getExtras().getString("gender");
 
 
-        button1 = (ToggleButton)findViewById(R.id.toggle_button1);
+        classToggleButton1 = (ToggleButton) findViewById(R.id.toggle_button1);
+        classToggleButton2 = (ToggleButton)findViewById(R.id.toggle_button2);
+        classToggleButton3 = (ToggleButton)findViewById(R.id.toggle_button3);
+        classToggleButton4 = (ToggleButton)findViewById(R.id.toggle_button4);
+        classToggleButton5 = (ToggleButton)findViewById(R.id.toggle_button5);
+        classToggleButton6 = (ToggleButton)findViewById(R.id.toggle_button6);
+        classToggleButton7 = (ToggleButton)findViewById(R.id.toggle_button7);
+        classToggleButton8 = (ToggleButton)findViewById(R.id.toggle_button8);
+        classToggleButton9 = (ToggleButton)findViewById(R.id.toggle_button9);
+        classToggleButton10 = (ToggleButton)findViewById(R.id.toggle_button10);
+        classToggleButton11 = (ToggleButton)findViewById(R.id.toggle_button11);
+
+
+
         signup_finish = (Button)findViewById(R.id.signup_finish);
         signupback = (ImageButton)findViewById(R.id.backbtn2);
         signupback.setOnClickListener(new View.OnClickListener() {
@@ -106,19 +122,30 @@ public class SignupActivity2 extends AppCompatActivity {
                 finish();
             }
         });
+
         signup_finish.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
-                    String result = new CustomTask().execute(email, pwd, name, gender, "join").get().toString();
+                    classCategory.clear();
+                    classCategoryString = "";
+                    getClassCatoeryCheked();
+                    Log.i("선택된카테고리", Integer.toString(classCategory.size()));
+                    for (int i = 0; i < classCategory.size(); i++)
+                    {
+                        classCategoryString += Integer.toString(classCategory.get(i));
+                        if (i != classCategory.size()-1) classCategoryString += ",";
+                    }
+                    Log.i("클래스카테고리 테스트", classCategoryString);
+                    String result = new CustomTask().execute(email, pwd, name, gender, classCategoryString, "join").get().toString();
 
                     if(result.equals("exist    ")) {
                         Toast.makeText(SignupActivity2.this,"이미 존재하는 아이디입니다.",Toast.LENGTH_SHORT).show();
                         finish();
                     } else if(result.equals("ok    ")) {
                         Log.i("실행여부", "ok");
-
                         Toast.makeText(SignupActivity2.this,"회원가입을 축하합니다.",Toast.LENGTH_SHORT).show();
+
                     }
 
                 } catch (Exception e) {
@@ -130,6 +157,21 @@ public class SignupActivity2 extends AppCompatActivity {
 
             }
         });
+
+    }
+
+    private void getClassCatoeryCheked() {
+        if (classToggleButton1.isChecked()) classCategory.add(1);
+        if (classToggleButton2.isChecked()) classCategory.add(2);
+        if (classToggleButton3.isChecked()) classCategory.add(3);
+        if (classToggleButton4.isChecked()) classCategory.add(4);
+        if (classToggleButton5.isChecked()) classCategory.add(5);
+        if (classToggleButton6.isChecked()) classCategory.add(6);
+        if (classToggleButton7.isChecked()) classCategory.add(7);
+         if (classToggleButton8.isChecked()) classCategory.add(8);
+         if (classToggleButton9.isChecked()) classCategory.add(9);
+         if (classToggleButton10.isChecked()) classCategory.add(10);
+         if (classToggleButton11.isChecked()) classCategory.add(11);
 
     }
 
